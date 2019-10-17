27ac23096bb6bdc1738225999704e233559d1acb
odmishien <tmtm.yagi.007@gmail.com>
2019-10-17 18:27:08 +0900

    gridSearch!!

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as p
 from sklearn import svm, metrics, preprocessing
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 
 data = p.read_csv(filepath_or_buffer='train.tsv', sep='\t')
 filtered_data = data[['age', 'race', 'sex', 'Y']]
@@ -24,16 +24,20 @@ sc.fit(pre_X)
 X = sc.transform(pre_X)
 Y = filtered_data['Y']
 
-svm_result = svm.LinearSVC(loss='hinge', C=1.0, class_weight='balanced')
-svm_result.fit(X, Y)
-
-scores = cross_val_score(svm_result, X, Y, cv=10)
-print("平均正解率 = ", scores.mean())
-print("正解率の標準偏差 = ", scores.std())
-
 X_train, X_test, train_label, test_label = train_test_split(X, Y, test_size=0.1, random_state=0)
-svm_result.fit(X_train, train_label)
-pre = svm_result.predict(X_test)
 
+parameters = [{'C': [0.01], 'class_weight': ['balanced'], 'gamma': [100], 'kernel': ['rbf']}]
+clf = GridSearchCV(svm.SVC(), parameters, n_jobs = -1)
+clf.fit(X_train, train_label)
+# svm_result = svm.LinearSVC(loss='hinge', C=1.0, class_weight='balanced')
+# svm_result.fit(X, Y)
+
+# scores = cross_val_score(svm_result, X, Y, cv=10)
+# print("平均正解率 = ", scores.mean())
+# print("正解率の標準偏差 = ", scores.std())
+
+
+pre = clf.predict(X_test)
 ac_score = metrics.accuracy_score(test_label,pre)
+
 print("正答率 = ",ac_score)
